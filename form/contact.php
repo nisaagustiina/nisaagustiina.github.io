80a9510d71dd4e50c1271263e7d940f34a96dc81
nisa <nisaagustiina@gmail.com>
2023-04-02 17:55:01 +0700

    update

--- form/contact.php
+++ form/contact.php
@@ -1,24 +1,17 @@
 <?php
 
-$receiving_email_address = 'nisaagustiina@gmail.com';
+$name =  $_POST['name'];
+$email =  $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-if(file_exists($php_email_form = '.../assets/vendor/php-email-form/php-email-form.php')){
-    include($php_email_form);
-}else{
-    die('Failed to connect!');
-}
+$to = "nisaagustiina@gmail.com";
+$body = "From : $email \n
+        Name : $name \n
+        About : $message";
 
-$contact = new PHP_EMAIL_FORM;
-$contact->ajax = true;
-$contact->to = $receiving_email_address;
-$contact->from_name = $_POST['name'];
-$contact->from_email = $_POST['email'];
-$contact->subject = $_POST['subject'];
+mail($to, $sub, $body);
 
-$contact->add_message($_POST['name'], 'From');
-$contact->add_message($_POST['email'], 'Email');
-$contact->add_message($_POST['message'], 'Message', 10);
-
-echo $contact->send();
+echo "Thank you to contact me!";
 
 ?>
